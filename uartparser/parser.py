997195f201724c71be7955e1bf905659347a788a
Eero af heurlin <eero.afheurlin@iki.fi>
2015-04-20 22:03:15 +0300

    I may have to go back to developing on pyboard directly so I will not accidentally use something that is not available there

--- uartparser/parser.py
+++ uartparser/parser.py
@@ -1,4 +1,4 @@
-# TODO: How to manage imports on cpython vs micropython
+# TODO: How to manage imports on cpython vs micropython (STM32 port [vs unix port])
 import uasyncio as asyncio
 
 from .errors import *

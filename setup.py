7f1610609076830d74a7821df8bbeec04574a1de
Eero af heurlin <eero.afheurlin@iki.fi>
2015-04-20 21:11:28 +0300

    initial stubs

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import subprocess
+
+git_version = 'UNKNOWN'
+try:
+    git_version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
+except subprocess.CalledProcessError,e:
+    #print "Got error when trying to read git version: %s" % e
+    pass
+
+setup(
+    name='scpi',
+    version='0.1.0dev-%s' % git_version,
+    #version='0.1.0',
+    author='Eero "rambo" af Heurlin',
+    author_email='rambo@iki.fi',
+    packages=[ 'uartparser', 'uartparser.errors', ],
+    license='GNU LGPL',
+    long_description=open('README.md').read(),
+    description='Implement universal UART parser in coroutines',
+    install_requires=open('requirements.txt').readlines(),
+    url='https://github.com/rambo/python-uartparser',
+)
